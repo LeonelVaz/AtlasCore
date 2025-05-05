d544cb02841cc701c4d2ae738bd2af4e608e14cd
Leonel Vazquez <leonelvazquezoficial@gmail.com>
2025-05-05 13:04:44 -0300

    refactor(vite.config): simplify configuration by removing unused options
    The `vite.config.js` file was simplified by removing unused and redundant configurations such as path aliases, build options, and server settings. This improves maintainability and reduces complexity.
    
    docs(comandos): add npm commands for cache cleanup and server start
    
    Added new npm-related commands to the documentation to assist developers in managing npm cache, reinstalling dependencies, and starting the server.

--- docs/dev/comandos.md
+++ docs/dev/comandos.md
@@ -7,3 +7,12 @@ tasklist /FI "IMAGENAME eq node.exe"
 
 ### eLiminar processos
 taskkill /F /IM node.exe
+
+### Elimina la caché de npm
+npm cache clean --force
+
+### Reinstala las dependencias
+npm install
+
+### Inicia el servidor
+npm start
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
   server: {
-    host: true, // Escucha en todas las direcciones de red, incluyendo 0.0.0.0
-    port: 5174,
-    strictPort: true, // Falla si el puerto ya está en uso
-    open: true, // Abre automáticamente el navegador
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
+    open: true
+  }
 });
